176d0b0c77e8c62e217f4610bc7763835e1037eb
Axel <you@example.com>
2022-11-07 01:39:28 +0100

    Added a header component with a custom logo

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <Header/>
+    </div>
+</template>
+
+
+
+
+
+
+<script>
+
+import Header from './components/Header.vue'
+
+export default {
+    name: 'App',
+    components: { 
+        Header
+    }
+}
+</script>
+
+
+
+
+<style scoped>
+    
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <img id="logo" src="../assets/images/ZarpCustomLogo.png">
+    </div> 
+</template>
+
+
+
+
+
+
+
+<script>
+export default {
+    name: 'Header',
+    components: {}
+}
+</script>
+
+
+
+<style scoped>
+
+.container {
+    height: 100px;
+    width: auto;
+
+    border: 2px solid #d4d4d4;
+    border-radius: 25px;
+
+    background-color: #447321;
+    opacity: 0.8;
+}
+
+#logo {
+    height: 85px;
+
+    padding-top: 8px;
+    padding-left: 10px;
+}
+
+
+</style>
